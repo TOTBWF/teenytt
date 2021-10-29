44bfb8aa4c60bbc3c885e3e9cd9d17cd0454287e
Reed Mullanix <reedmullanix@gmail.com>
2021-10-29 09:40:56 -0700

    Document 'Tactic.hs'

--- src/TeenyTT/Core/Tactic.hs
+++ src/TeenyTT/Core/Tactic.hs
@@ -2,6 +2,7 @@ module TeenyTT.Core.Tactic
   ( Chk(..)
   , Syn(..)
   , Tp(..)
+  -- * Tactic Conversion
   , chk
   , ann
   -- * Variable Tactics
@@ -19,21 +20,33 @@ import TeenyTT.Core.Eval
 import TeenyTT.Core.Domain qualified as D
 import TeenyTT.Core.Syntax qualified as S
 
+-- | A @Chk@ tactic can be thought of as a tactic that does nothing
+-- more than mechanical decomposition.
 newtype Chk = Chk { runChk :: D.Type -> RM S.Term }
 
+-- | A @Syn@ tactic can be thought of as 2 classes of tactics:
+-- * Things like variable lookup that already "know" their type
+-- * Things like function application that need to "invent" information.
 newtype Syn = Syn { runSyn :: RM (S.Term, D.Type) }
 
+-- | A @Tp@ tactic is used to synthesize types, rather than any sort of term.
+-- These correspond to the type formation rules.
 newtype Tp = Tp { runTp :: RM S.Type }
 
 --------------------------------------------------------------------------------
 -- Tactic Conversion
 
+-- | @chk tac@ will convert a 'Syn' tactic into a 'Chk' tactic by
+-- synthesizing something, and checking that the synthesized type
+-- is the same as the one we are checking against.
 chk :: Syn -> Chk
 chk t = Chk $ \goal -> do
     (tm, tp) <- runSyn t
     liftConv $ equateTp goal tp
     pure tm
 
+-- | @ann tac tp@ takes a 'Chk' tactic and transforms it into a 'Syn' one
+-- by providing it with a sort of "type annotation".
 ann :: Chk -> Tp -> Syn
 ann tac tpTac = Syn $ do
     tp <- runTp tpTac
@@ -42,8 +55,9 @@ ann tac tpTac = Syn $ do
     pure (tm, vtp)
 
 --------------------------------------------------------------------------------
--- Variable Tactics
+-- Variables
 
+-- | A 'Var' is used to ensure that we handle binding structures properly.
 data Var = Var { vtp :: D.Type, vtm :: D.Value }
 
 abstract :: Ident -> D.Type -> (Var -> RM a) -> RM a
